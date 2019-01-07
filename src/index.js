2a93ef4348cf4a9baf79e83035070de9522c468b
Pete Turnbull <pete.j.turnbull@gmail.com>
2019-01-07 03:03:14 +0100

    simplified internals of cron

--- src/index.js
+++ src/index.js
@@ -1,74 +1,48 @@
 const moment = require('moment')
 require('moment-timezone')
-const nSchedule = require('node-schedule-tz')
-const EventEmitter = require('events').EventEmitter
 
 const HOUR_IN_MS = 60 * 60 * 1000
 const DAY_IN_MS = 24 * HOUR_IN_MS
-
-const log = () => {}
 
 function startInterval(secs, cb) {
   cb()
   return setInterval(cb, secs * 1000)
 }
 
-function calcCronExpr(mode, number) {
-  if (mode === 'HOURLY') {
-    return `0 ${number} * * * *`
-  } else {
-    return `0 0 ${number} * * *`
-  }
-}
-
 class CronDaemon {
-  constructor(getDocs, triggerDoc, { mode, number, logger } = {}) {
-    this.log = logger || log
-    this.jobDocs = {}
-    this.jobMap = {}
+  constructor(getDocs, triggerDoc, { mode, number } = {}) {
+    this.docMap = {}
 
     this.getDocs = getDocs
     this.triggerDoc = triggerDoc
     this.opts = { mode, number: number || 0 }
 
     this.syncDocs = this.syncDocs.bind(this)
-    this.scanForFailedPublishes = this.scanForFailedPublishes.bind(this)
-    this.setupJob = this.setupJob.bind(this)
-    this.removeJob = this.removeJob.bind(this)
+    this.scanDocs = this.scanDocs.bind(this)
     this.trigger = this.trigger.bind(this)
   }
 
   async start() {
     startInterval(60, this.syncDocs)
-    startInterval(10, this.scanForFailedPublishes)
+    startInterval(15, this.scanDocs)
   }
 
   async syncDocs() {
-    this.log('Syncing docs.')
+    const docs = await this.getDocs()
+    const docMap = {}
 
-    const current = Object.keys(this.jobDocs).map(k => this.jobDocs[k])
-    const latest = await this.getDocs()
+    docs.forEach(doc => {
+      docMap[doc.id] = doc
+    })
 
-    const cIds = new Set(current.map(d => d.id))
-    const nIds = new Set(latest.map(d => d.id))
-
-    const disabledIds = Array.from(new Set([...cIds].filter(x => !nIds.has(x))))
-
-    const newDocs = Array.from(
-      new Set([...nIds].filter(x => !cIds.has(x))),
-    ).map(id => latest.filter(d => d.id === id)[0])
-
-    disabledIds.forEach(this.removeJob)
-    newDocs.forEach(this.setupJob)
+    this.docMap = docMap
   }
 
-  async scanForFailedPublishes() {
-    this.log('Scanning for failed publishes.')
+  async scanDocs() {
+    const docs = Object.keys(this.docMap).map(k => this.docMap[k])
+    const toTrigger = []
 
-    const issues = []
-    const docList = Object.keys(this.jobDocs).map(k => this.jobDocs[k])
-
-    docList.forEach(doc => {
+    docs.forEach(doc => {
       const timezone = doc.timezone || 'UTC'
       const nowLocal = moment.utc(
         moment.tz(timezone).format('YYYY-MM-DDTHH:mm:ss'),
@@ -85,13 +59,11 @@ class CronDaemon {
             .add(1, 'hour')
             .startOf('hour')
             .subtract(60 - number, 'minutes')
-            .add(5, 'minutes')
         } else {
           upperCP = checkpointM
             .add(1, 'hour')
             .startOf('hour')
             .add(number, 'minutes')
-            .add(5, 'minutes')
         }
 
         const diffInHours = Math.floor(nowLocal.diff(upperCP) / HOUR_IN_MS)
@@ -103,43 +75,21 @@ class CronDaemon {
             .add(1, 'day')
             .startOf('day')
             .subtract(24 - number, 'hours')
-            .add(5, 'minutes')
         } else {
           upperCP = checkpointM
             .add(1, 'day')
             .startOf('day')
             .add(number, 'hours')
-            .add(5, 'minutes')
         }
 
         const diffInDays = Math.floor(nowLocal.diff(upperCP) / DAY_IN_MS)
         mCount = Math.max(diffInDays + 1, 0)
       }
 
-      if (doc.runSince < mCount) issues.push(doc)
+      if (doc.runSince < mCount) toTrigger.push(doc)
     })
 
-    await Promise.all(issues.map(d => this.trigger(d, true)))
-  }
-
-  setupJob(doc) {
-    const f = () => this.trigger(doc, false)
-    const number = doc.number || this.opts.number || 0
-    const cronExpr = calcCronExpr(this.opts.mode, number)
-    const timezone = doc.timezone || 'UTC'
-
-    this.log(`Setting up job: ${doc.id} - ${cronExpr} - ${timezone}.`)
-
-    this.jobDocs[doc.id] = doc
-    this.jobMap[doc.id] = nSchedule.scheduleJob(doc.id, cronExpr, timezone, f)
-  }
-
-  removeJob(id) {
-    this.log(`Removing job: ${id}.`)
-
-    this.jobMap[id].cancel()
-    delete this.jobMap[id]
-    delete this.jobDocs[id]
+    await Promise.all(toTrigger.map(d => this.trigger(d, true)))
   }
 
   async trigger(doc, isRetry) {
@@ -148,10 +98,8 @@ class CronDaemon {
       ? { checkpoint: now, runSince: 0 }
       : { runSince: doc.runSince + 1 }
 
-    this.log(`Triggering job: ${doc.id} - isRetry[${isRetry}].`)
-
     await this.triggerDoc(doc.id, data)
-    this.jobDocs[doc.id] = Object.assign(doc, data)
+    this.docMap[doc.id] = Object.assign(doc, data)
   }
 }
 
